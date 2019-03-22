0c8c1a2716d650211d735022af3c65b683196dd1
Str4ve <48835851+Str4ve@users.noreply.github.com>
2019-03-22 19:45:44 +0100

    bild

---
+++ core/src/infks2/spacegame/SpaceShip/Block.java
@@ -0,0 +1,14 @@
+package infks2.spacegame.SpaceShip;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Block {
+    public Texture texture;
+
+
+    public Block() {
+        this.texture = new Texture("SpaceGame_Block.png");
+    }
+}
