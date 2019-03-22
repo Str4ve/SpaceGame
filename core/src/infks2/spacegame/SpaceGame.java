18628cd600927d6fcfef9d324a51ebce31bc6fac
AleMax <alemaxsng@gmail.com>
2019-03-22 19:34:05 +0100

    test

--- core/src/infks2/spacegame/SpaceGame.java
+++ core/src/infks2/spacegame/SpaceGame.java
@@ -5,15 +5,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import infks2.spacegame.SpaceShip.Blocks;
 
 public class SpaceGame extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
+	Blocks block;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("image/IMG_0021.PNG");
+		block = new Blocks();
 	}
 
 	@Override
@@ -22,6 +26,7 @@ public class SpaceGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(block.texture, 0, 0);
 		batch.end();
 		//ja brudiiiii
 	}
