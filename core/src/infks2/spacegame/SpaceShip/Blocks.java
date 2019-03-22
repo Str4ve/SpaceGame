8943e6a843a54d438dc2b65d4012675ff5ecf00d
AleMax <alemaxsng@gmail.com>
2019-03-22 19:44:16 +0100

    test

--- core/src/infks2/spacegame/SpaceShip/Blocks.java
+++ core/src/infks2/spacegame/SpaceShip/Blocks.java
@@ -1,4 +1,13 @@
 package infks2.spacegame.SpaceShip;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public class Blocks {
+
+    public Texture texture;
+
+    public Blocks() {
+        this.texture = new Texture("badlogic.jpg");
+    }
+
 }
